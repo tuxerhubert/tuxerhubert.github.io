178df08981db4222a81069046d58c8de9960c924
Hubert Baumann <baumann.hubert.jun@gmail.com>
2024-09-09 00:29:45 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<!-- these 3 meta tags are required for bootstrap and must be placed on top of head -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>tuxerhubert</title>
+	<!-- Bootstrap-CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<!-- additional styles insert here -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<style>
+	</style>
+</head>
+<!-- sm style used -->
+<!-- xs (phones), sm (tablets), md (desktops), and lg (larger desktops) -->
+<body>
+
+
+	<!-- body will be loaded faster in case all scripts are located on bottom -->
+	<!-- bootstrap scripts-->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+	<!-- my JS scripts -->
+	<script>
+		body.onload = function () {
+	
+		}
+	</script>
+</body>
+</html>
